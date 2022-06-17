2664ff18ddca4b631d0cc08a06b8fd2c5bd80816
shkim <abkorc33@gmail.com>
2022-06-17 13:32:38 +0900

    first commit

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+declare namespace ReactNavigation {
+    interface RootParamList {
+        Home: undefined
+        HomeLeft: undefined
+        HomeRight: {name: string; age: Number}
+        HomeNavigator: undefined
+        Login: undefined
+        SignUp: undefined
+        TabNavigator: undefined
+    }
+}
---
+++ src/hooks/useClock.ts
@@ -0,0 +1,13 @@
+import { time } from 'faker'
+import {useState, useEffect} from 'react'
+
+export const useClock = () => {
+    const [time, setTime] = useState(new Date())
+    useEffect(() => {
+        const id = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+        return () => clearInterval(id)
+    }, [])
+    return time
+}
