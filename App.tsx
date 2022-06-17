46fdf8cf79598fd01560ba6a5e039685dc98a5bf
shkim <abkorc33@gmail.com>
2022-06-17 13:24:07 +0900

    7-5

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useCallback } from 'react';
+import 'react-native-gesture-handler'
+import { enableScreens } from 'react-native-screens'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native'
+import { useColorScheme } from 'react-native'
+
+import { ToggleThemeProvider } from './src/contexts'
+import MainNavigator from './src/screens/MainNavigator'
+
+enableScreens()
+
+export default function App() {
+  const scheme = useColorScheme()
+  const [theme, setTheme] = useState(
+    scheme == 'dark' ? DarkTheme : DefaultTheme
+  )
+  const toggleTheme = useCallback(
+    () => setTheme(({ dark }) => (dark ? DefaultTheme : DarkTheme)),
+    [])
+
+  return (
+    <ToggleThemeProvider toggleTheme={toggleTheme}>
+      <SafeAreaProvider>
+        <NavigationContainer theme={theme}>
+          <MainNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </ToggleThemeProvider>
+  )
+}
